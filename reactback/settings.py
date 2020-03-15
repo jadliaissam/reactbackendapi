85c15b7962811af2d7df3919efcb08a8d55b5d31
Aissam <hesokaslave@gmail.com>
2020-03-15 20:56:43 +0100

    fix static

--- reactback/settings.py
+++ reactback/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SQL_LITE = {
     'default': {
